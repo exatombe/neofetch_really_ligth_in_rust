c29c2b0e14851aea0babebd5d28dcadacf00848b
garder500 <jeremy27.clara22@gmail.com>
2023-07-04 12:42:08 +0200

    Update (adding colors)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use sysinfo::{System, SystemExt, UserExt, ProcessExt, Uid, CpuExt};
-
+use colored::{Colorize, Color};
 fn main(){
     let mut sys: System = System::new_all();
 
@@ -85,21 +85,20 @@ let pid = sysinfo::get_current_pid().unwrap();
 let process = sys.process(pid).unwrap();
 
 let user = sys.get_user_by_id(&option_to_uid(process.user_id()));
-println!("{}@{}", user.unwrap().name(), option_to_string(sys.host_name().as_deref()));
+println!("{}{}{}", user.unwrap().name().blue(),"@".blue(),option_to_string(sys.host_name().as_deref()).blue());
  // Display system information:
-println!("OS:           {}", option_to_string(sys.long_os_version().as_deref()));
-println!("Kernel:       {}", option_to_string(sys.os_version().as_deref()));
-println!("Uptime:       {}", convert_second_to_humantime(sys.uptime()));
-println!("Processes:    {}", sys.processes().len());
-println!("Users:        {}", sys.users().len());
-println!("Boot time:    {}", convert_second_to_humantime(sys.boot_time()));
+println!("{}           {}","OS:".blue(), option_to_string(sys.long_os_version().as_deref()));
+println!("{}       {}","Kernel:".blue(), option_to_string(sys.os_version().as_deref()));
+println!("{}       {}","Uptime:".blue(), convert_second_to_humantime(sys.uptime()));
+println!("{}    {}","Processes:".blue(), sys.processes().len());
+println!("{}        {}","Users:".blue(),sys.users().len());
+println!("{}    {}","Boot time:".blue(), convert_second_to_humantime(sys.boot_time()));
 sys.refresh_cpu(); // Refreshing CPU information.
 for cpu in sys.cpus() {
-    println!("{}% ", cpu.brand());
+    println!("{}       {}","CPU:".blue(),cpu.brand());
     break;
 }
-println!("Memory: {} Mib / {} Mib", sys.used_memory()/1024/1024, sys.total_memory()/1024/1024);
-
+println!("{} {} Mib / {} Mib","Memory:".blue(), sys.used_memory()/1024/1024, sys.total_memory()/1024/1024);
 
 
 }
